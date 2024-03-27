788efbabadb73537996a00291c28fff7fdff947d
Søren Granfeldt <l-sg@regsj.dk>
2024-03-27 11:04:03 +0100

    Changed schema to interpret types from Dictionary Optimized debug message

--- Granfeldt.PowerShell.ManagementAgent/MA.Schema.cs
+++ Granfeldt.PowerShell.ManagementAgent/MA.Schema.cs
@@ -9,150 +9,144 @@ using System.Text.RegularExpressions;
 
 namespace Granfeldt
 {
-	public partial class PowerShellManagementAgent : IDisposable, IMAExtensible2GetCapabilities, IMAExtensible2GetSchema, IMAExtensible2GetParameters, IMAExtensible2CallImport, IMAExtensible2CallExport, IMAExtensible2Password
-	{
-		Collection<PSObject> schemaResults;
-		string SchemaScript = null;
+    public partial class PowerShellManagementAgent : IDisposable, IMAExtensible2GetCapabilities, IMAExtensible2GetSchema, IMAExtensible2GetParameters, IMAExtensible2CallImport, IMAExtensible2CallExport, IMAExtensible2Password
+    {
+        Collection<PSObject> schemaResults;
+        string SchemaScript = null;
 
-		class AttributeDefinition
-		{
-			public string Name { get; set; }
-			public AttributeType Type { get; set; }
-			public bool IsMultiValue { get; set; }
-			public bool IsAnchor { get; set; }
-			public bool ImportOnly { get; set; }
-			public bool ExportOnly { get; set; }
-		}
+        class AttributeDefinition
+        {
+            public string Name { get; set; }
+            public AttributeType Type { get; set; }
+            public bool IsMultiValue { get; set; }
+            public bool IsAnchor { get; set; }
+            public bool ImportOnly { get; set; }
+            public bool ExportOnly { get; set; }
+        }
 
-		Schema IMAExtensible2GetSchema.GetSchema(KeyedCollection<string, ConfigParameter> configParameters)
-		{
-			Tracer.Enter("getschema");
-			try
-			{
-				Schema schema = Schema.Create();
-				InitializeConfigParameters(configParameters);
+        // define a dictionary to map attribute types to AttributeType enum values
+        Dictionary<string, AttributeType> typeMappings = new Dictionary<string, AttributeType>
+        {
+            { "boolean", AttributeType.Boolean },
+            { "binary", AttributeType.Binary },
+            { "integer", AttributeType.Integer },
+            { "reference", AttributeType.Reference },
+            { "string", AttributeType.String }
+        };
 
-				OpenRunspace();
-				Command cmd = new Command(Path.GetFullPath(SchemaScript));
-				cmd.Parameters.Add(new CommandParameter("Username", Username));
-				cmd.Parameters.Add(new CommandParameter("Password", Password));
-				cmd.Parameters.Add(new CommandParameter("Credentials", GetSecureCredentials(Username, SecureStringPassword)));
+        Schema IMAExtensible2GetSchema.GetSchema(KeyedCollection<string, ConfigParameter> configParameters)
+        {
+            Tracer.Enter("getschema");
+            try
+            {
+                Schema schema = Schema.Create();
+                InitializeConfigParameters(configParameters);
 
-				cmd.Parameters.Add(new CommandParameter("AuxUsername", UsernameAux));
-				cmd.Parameters.Add(new CommandParameter("AuxPassword", PasswordAux));
-				cmd.Parameters.Add(new CommandParameter("AuxCredentials", GetSecureCredentials(UsernameAux, SecureStringPasswordAux)));
+                OpenRunspace();
+                Command cmd = new Command(Path.GetFullPath(SchemaScript));
+                cmd.Parameters.Add(new CommandParameter("Username", Username));
+                cmd.Parameters.Add(new CommandParameter("Password", Password));
+                cmd.Parameters.Add(new CommandParameter("Credentials", GetSecureCredentials(Username, SecureStringPassword)));
 
-				cmd.Parameters.Add(new CommandParameter("ConfigurationParameter", ConfigurationParameter));
+                cmd.Parameters.Add(new CommandParameter("AuxUsername", UsernameAux));
+                cmd.Parameters.Add(new CommandParameter("AuxPassword", PasswordAux));
+                cmd.Parameters.Add(new CommandParameter("AuxCredentials", GetSecureCredentials(UsernameAux, SecureStringPasswordAux)));
 
-				schemaResults = InvokePowerShellScript(cmd, null);
-				CloseRunspace();
+                cmd.Parameters.Add(new CommandParameter("ConfigurationParameter", ConfigurationParameter));
 
-				if (schemaResults != null)
-				{
-					foreach (PSObject obj in schemaResults)
-					{
-						string objectTypeName = null;
-						HashSet<AttributeDefinition> attrs = new HashSet<AttributeDefinition>();
+                schemaResults = InvokePowerShellScript(cmd, null);
+                CloseRunspace();
 
-						foreach (PSPropertyInfo p in obj.Properties)
-						{
-							string[] elements = p.Name.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-							string attrName = elements[0].Trim();
-							string attrType = elements[1].Trim();
+                if (schemaResults != null)
+                {
+                    foreach (PSObject obj in schemaResults)
+                    {
+                        string objectTypeName = null;
+                        HashSet<AttributeDefinition> attrs = new HashSet<AttributeDefinition>();
 
-							if (string.Equals(attrName, Constants.ControlValues.ObjectClass, StringComparison.OrdinalIgnoreCase))
-							{
-								objectTypeName = p.Value.ToString();
-								Tracer.TraceInformation("object-class '{0}'", objectTypeName);
-							}
-							else
-							{
-								AttributeDefinition ad = new AttributeDefinition();
-								ad.Name = Regex.Replace(attrName, "^Anchor-", "", RegexOptions.IgnoreCase);
-								ad.Name = Regex.Replace(ad.Name, "^ImportOnly-", "", RegexOptions.IgnoreCase);
-								ad.Name = Regex.Replace(ad.Name, "^ExportOnly-", "", RegexOptions.IgnoreCase);
-								ad.IsAnchor = p.Name.StartsWith("anchor-", StringComparison.OrdinalIgnoreCase);
-								ad.ImportOnly = p.Name.StartsWith("importonly-", StringComparison.OrdinalIgnoreCase);
-								ad.ExportOnly = p.Name.StartsWith("exportonly-", StringComparison.OrdinalIgnoreCase);
-								ad.IsMultiValue = p.Name.EndsWith("[]", StringComparison.OrdinalIgnoreCase);
-								switch (attrType.Replace("[]", "").ToLower())
-								{
-									case "boolean":
-										ad.Type = AttributeType.Boolean;
-										break;
-									case "binary":
-										ad.Type = AttributeType.Binary;
-										break;
-									case "integer":
-										ad.Type = AttributeType.Integer;
-										break;
-									case "reference":
-										ad.Type = AttributeType.Reference;
-										break;
-									case "string":
-										ad.Type = AttributeType.String;
-										break;
-									default:
-										ad.Type = AttributeType.String;
-										break;
-								}
-								Tracer.TraceInformation("name '{0}', isanchor: {1}, ismultivalue: {2}, importonly: {3}, exportonly: {4}, type: {5}", ad.Name, ad.IsAnchor, ad.IsMultiValue, ad.ImportOnly, ad.ExportOnly, ad.Type.ToString());
-								attrs.Add(ad);
-							}
-						}
-						if (string.IsNullOrEmpty(objectTypeName))
-						{
-							Tracer.TraceError("missing-object-class");
-							throw new Microsoft.MetadirectoryServices.NoSuchObjectTypeException();
-						}
+                        foreach (PSPropertyInfo p in obj.Properties)
+                        {
+                            string[] elements = p.Name.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                            string attrName = elements[0].Trim();
+                            string attrType = elements[1].Trim();
 
-						SchemaType objectClass = SchemaType.Create(objectTypeName, true);
-						foreach (AttributeDefinition def in attrs)
-						{
-							if (def.IsAnchor)
-							{
-								objectClass.Attributes.Add(SchemaAttribute.CreateAnchorAttribute(def.Name, def.Type));
-							}
-							else
-							{
-								var attrOperation = def.ExportOnly ? AttributeOperation.ExportOnly :
-													def.ImportOnly ? AttributeOperation.ImportOnly : AttributeOperation.ImportExport;
+                            if (string.Equals(attrName, Constants.ControlValues.ObjectClass, StringComparison.OrdinalIgnoreCase))
+                            {
+                                objectTypeName = p.Value.ToString();
+                                Tracer.TraceInformation($"object-class '{objectTypeName}'");
+                            }
+                            else
+                            {
+                                AttributeDefinition ad = new AttributeDefinition();
+                                ad.Name = Regex.Replace(attrName, "^(Anchor-|ImportOnly-|ExportOnly-)", "", RegexOptions.IgnoreCase);
 
-								if (def.IsMultiValue)
-								{
-									objectClass.Attributes.Add(SchemaAttribute.CreateMultiValuedAttribute(def.Name, def.Type, attrOperation));
-								}
-								else
-								{
-									objectClass.Attributes.Add(SchemaAttribute.CreateSingleValuedAttribute(def.Name, def.Type, attrOperation));
-								}
-							}
-						}
-						if (objectClass.AnchorAttributes.Count == 1)
-						{
-							schema.Types.Add(objectClass);
-						}
-						else
-						{
-							Tracer.TraceError("missing-anchor-definition-on-object");
-							throw new Microsoft.MetadirectoryServices.AttributeNotPresentException();
-						}
-					}
-				}
-				schemaResults.Clear();
-				return schema;
-			}
-			catch (Exception ex)
-			{
-				Tracer.TraceError("getschema", ex);
-				throw;
-			}
-			finally
-			{
-				Tracer.Exit("getschema");
-			}
-		}
+                                ad.IsAnchor = p.Name.StartsWith("anchor-", StringComparison.OrdinalIgnoreCase);
+                                ad.ImportOnly = p.Name.StartsWith("importonly-", StringComparison.OrdinalIgnoreCase);
+                                ad.ExportOnly = p.Name.StartsWith("exportonly-", StringComparison.OrdinalIgnoreCase);
+                                ad.IsMultiValue = p.Name.EndsWith("[]", StringComparison.OrdinalIgnoreCase);
 
-	}
+                                // get the attribute type without array notation and convert to lowercase and
+                                // set ad.Type based on the dictionary mapping or default to AttributeType.String
+                                string cleanedAttrType = attrType.Replace("[]", "").ToLower();
+                                ad.Type = typeMappings.ContainsKey(cleanedAttrType) ? typeMappings[cleanedAttrType] : AttributeType.String;
+
+                                Tracer.TraceInformation($"name '{ad.Name}', isanchor: {ad.IsAnchor}, ismultivalue: {ad.IsMultiValue}, importonly: {ad.ImportOnly}, exportonly: {ad.ExportOnly}, type: {ad.Type}"); //, ad.Name, ad.IsAnchor, ad.IsMultiValue, ad.ImportOnly, ad.ExportOnly, ad.Type.ToString());
+                                attrs.Add(ad);
+                            }
+                        }
+                        if (string.IsNullOrEmpty(objectTypeName))
+                        {
+                            Tracer.TraceError("missing-object-class");
+                            throw new NoSuchObjectTypeException();
+                        }
+
+                        SchemaType objectClass = SchemaType.Create(objectTypeName, true);
+                        foreach (AttributeDefinition def in attrs)
+                        {
+                            if (def.IsAnchor)
+                            {
+                                objectClass.Attributes.Add(SchemaAttribute.CreateAnchorAttribute(def.Name, def.Type));
+                            }
+                            else
+                            {
+                                var attrOperation = def.ExportOnly ? AttributeOperation.ExportOnly :
+                                                    def.ImportOnly ? AttributeOperation.ImportOnly : AttributeOperation.ImportExport;
+
+                                if (def.IsMultiValue)
+                                {
+                                    objectClass.Attributes.Add(SchemaAttribute.CreateMultiValuedAttribute(def.Name, def.Type, attrOperation));
+                                }
+                                else
+                                {
+                                    objectClass.Attributes.Add(SchemaAttribute.CreateSingleValuedAttribute(def.Name, def.Type, attrOperation));
+                                }
+                            }
+                        }
+                        if (objectClass.AnchorAttributes.Count == 1)
+                        {
+                            schema.Types.Add(objectClass);
+                        }
+                        else
+                        {
+                            Tracer.TraceError("missing-anchor-definition-on-object");
+                            throw new AttributeNotPresentException();
+                        }
+                    }
+                }
+                schemaResults.Clear();
+                return schema;
+            }
+            catch (Exception ex)
+            {
+                Tracer.TraceError("getschema", ex);
+                throw;
+            }
+            finally
+            {
+                Tracer.Exit("getschema");
+            }
+        }
+
+    }
 
 }
